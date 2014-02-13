f3958fe09a25030e05beebf6b590bcac00bd2e7d
hagaygo <hagay.goshen@gmail.com>
2014-02-13 20:53:51 +0200

    added progressbar component

---
+++ WindowsWebAdmin/Models/ProgressData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WindowsWebAdmin.Models
+{
+    public class ProgressData
+    {
+        public ProgressData()
+        {
+            WidthInPixels = 150;
+            HeightInPixels = 20;
+        }
+        public int HeightInPixels { get; set; }
+        public int WidthInPixels { get; set; }
+        public ulong Value { get; set; }
+        public ulong Max { get; set; }
+    }
+}
