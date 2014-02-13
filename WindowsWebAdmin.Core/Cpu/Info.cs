ffdbf35e8784cf92e8581ef13f073f75bcb392d9
hagaygo <hagay.goshen@gmail.com>
2014-02-13 12:55:30 +0200

    machine stat info

---
+++ WindowsWebAdmin.Core/Cpu/Info.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WindowsWebAdmin.Core.Cpu
+{
+    public class Info
+    {
+        public int Cores { get; set; }
+        public decimal Speed { get; set; }
+        public decimal CurrentSpeed { get; set; }
+        public string Name { get; set; }
+        public int Load { get; set; }
+    }
+}
---
+++ WindowsWebAdmin.Core/WinApi/Cpu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsWebAdmin.Core.Cpu;
+
+namespace WindowsWebAdmin.Core.WinApi
+{
+    public static class Cpu
+    {
+        public static List<Info> GetCpuInfo()
+        {
+            var lst = new List<Info>();
+            var searcher = new ManagementObjectSearcher("select * from Win32_Processor");
+            foreach (ManagementObject s in searcher.Get())
+            {
+                var i = new Info();
+                i.Name = s.GetPropertyValue("Name").ToString();
+                i.Cores = Convert.ToInt32(s.GetPropertyValue("NumberOfCores").ToString());
+                i.Speed = Convert.ToDecimal(s.GetPropertyValue("MaxClockSpeed").ToString());
+                i.CurrentSpeed = Convert.ToDecimal(s.GetPropertyValue("CurrentClockSpeed").ToString());
+                i.Load = Convert.ToInt32(s.GetPropertyValue("LoadPercentage").ToString());
+                lst.Add(i);                
+            }
+
+            return lst;
+        }
+    }
+}
