62589a57e7cf50ebdbe1b860e0b21b776765393e
hagaygo <hagay.goshen@gmail.com>
2014-02-14 13:05:22 +0200

    basic disk info prepartion

---
+++ WindowsWebAdmin/Controllers/DiskController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WindowsWebAdmin.Controllers
+{
+    public class DiskController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return Info();
+        }
+
+        public ActionResult Info()
+        {
+            var i = new WindowsWebAdmin.Models.Disk();
+            i.FillBaseData();
+            return View(i);
+        }
+	}
+}
